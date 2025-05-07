f80c779e48d1b62876caf7215e1236f2ceed39b2
muhammadnuribnuhubab <muhammadnuribnuhubab@gmail.com>
2025-05-07 09:47:19 +0700

    feat: add color pallete and font Poppins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-extrabold underline text-neutral-400">
       Hello world!
     </h1>
   )
